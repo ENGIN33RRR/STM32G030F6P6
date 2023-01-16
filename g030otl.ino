3a9eb14f38a1e6dcb0586e51de2183a8cca93236
ENGIN33RRR <113207385+ENGIN33RRR@users.noreply.github.com>
2023-01-17 00:27:23 +0600

    Отладочная плата

---
+++ g030otl.ino
@@ -0,0 +1,98 @@
+int DSPIN = PA5;
+
+//                      RX    TX
+HardwareSerial Serial1(PB7, PB6);  //привязываем порты к USART
+
+void setup() {
+
+  Serial1.begin(9600);    //инициализация USART1
+
+}
+
+void loop() {
+  delay(500);
+
+  double temp = TempRead();
+  temp  = temp * 0.0625; // conversion accuracy is 0.0625 / LSB
+  Serial1.print("Temperature: ");
+  Serial1.print(temp);
+  Serial1.println(" °C");
+  Serial1.println("");
+}
+
+
+
+
+
+boolean DS18B20_Init()   
+{
+  pinMode(DSPIN, OUTPUT);
+  digitalWrite(DSPIN, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(DSPIN, LOW);
+  delayMicroseconds(750);//480-960
+  digitalWrite(DSPIN, HIGH);
+  pinMode(DSPIN, INPUT);
+  int t = 0;
+  while (digitalRead(DSPIN))
+  {
+    t++;
+    if (t > 60) return false;
+    delayMicroseconds(1);
+  }
+  t = 480 - t;
+  pinMode(DSPIN, OUTPUT);
+  delayMicroseconds(t);
+  digitalWrite(DSPIN, HIGH);
+  return true;
+}
+
+void DS18B20_Write(byte data)
+{
+  pinMode(DSPIN, OUTPUT);
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(DSPIN, LOW);
+    delayMicroseconds(10);
+    if (data & 1) digitalWrite(DSPIN, HIGH);
+    else digitalWrite(DSPIN, LOW);
+    data >>= 1;
+    delayMicroseconds(50);
+    digitalWrite(DSPIN, HIGH);
+  }
+}
+
+byte DS18B20_Read()
+{
+  pinMode(DSPIN, OUTPUT);
+  digitalWrite(DSPIN, HIGH);
+  delayMicroseconds(2);
+  byte data = 0;
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(DSPIN, LOW);
+    delayMicroseconds(1);
+    digitalWrite(DSPIN, HIGH);
+    pinMode(DSPIN, INPUT);
+    delayMicroseconds(5);
+    data >>= 1;
+    if (digitalRead(DSPIN)) data |= 0x80;
+    delayMicroseconds(55);
+    pinMode(DSPIN, OUTPUT);
+    digitalWrite(DSPIN, HIGH);
+  }
+  return data;
+}
+
+int TempRead()
+{
+  if (!DS18B20_Init()) return 0;
+  DS18B20_Write (0xCC); // Send skip ROM command
+  DS18B20_Write (0x44); // Send reading start conversion command
+  if (!DS18B20_Init()) return 0;
+  DS18B20_Write (0xCC); // Send skip ROM command
+  DS18B20_Write (0xBE); // Read the register, a total of nine bytes, the first two bytes are the conversion value
+  int temp = DS18B20_Read (); // Low byte
+  temp |= DS18B20_Read () << 8; // High byte
+  return temp;
+}
